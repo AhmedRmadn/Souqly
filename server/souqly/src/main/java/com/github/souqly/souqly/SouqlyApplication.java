9180fae5f27b737b72d6123a7696c1cd3f73c812
Ahmed Ramadan <ahmednasser1902@gmail.com>
2025-07-13 18:24:54 +0300

    init souqly

---
+++ server/souqly/src/main/java/com/github/souqly/souqly/SouqlyApplication.java
@@ -0,0 +1,13 @@
+package com.github.souqly.souqly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouqlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouqlyApplication.class, args);
+	}
+
+}
